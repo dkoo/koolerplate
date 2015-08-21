ee610291f5f15bf387553469238b6ee52f7c646b
dkoo <derrick_koo@newyorker.com>
2015-08-20 23:15:54 -0400

    proper build task dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ gulp.task('lint', function() {
 });
 
 /* bundle js modules */
-gulp.task('browserify', function() {
+gulp.task('browserify', ['lint'], function() {
 	return browserify('./app/js/app.js')
 		.bundle()
 		.pipe(source('app.js'))
@@ -40,13 +40,26 @@ gulp.task('sass', function() {
 });
 
 /* post-process CSS with autoprefixer */
-gulp.task('autoprefix', function() {
+gulp.task('autoprefix', ['sass'], function() {
 	return gulp.src('./build/css/unprefixed/styles.css')
 		.pipe(autoprefixer({
 			browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1'],
 			cascade: false
 		}))
 		.pipe(gulp.dest('./dist/css/'));
+});
+
+/* build js */
+gulp.task('js', ['lint', 'browserify']);
+
+/* build everything */
+gulp.task('build', ['sass', 'autoprefix', 'js']);
+
+/* create a simple http server at localhost:8080 */
+gulp.task('server', function(done) {
+	http.createServer(
+		st({ path: __dirname + '/', index: 'index.html', cache: false })
+	).listen(8080, done);
 });
 
 /* watch files for changes and execute tasks */
@@ -56,14 +69,5 @@ gulp.task('watch', ['server'], function() {
 	gulp.watch('./app/js/**/*.js', ['js']);
 });
 
-/* build js */
-gulp.task('js', ['lint', 'browserify']);
-
 /* default gulp task */
 gulp.task('default', ['watch']);
-
-gulp.task('server', function(done) {
-	http.createServer(
-		st({ path: __dirname + '/', index: 'index.html', cache: false })
-	).listen(8080, done);
-});
